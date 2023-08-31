191bea2217d587dfac3a9ea60aa4f517ab7278dc
Anna Wójcik <aniawojcik894@gmail.com>
2023-08-31 14:34:49 +0200

    task list without interactions yet

---
+++ src/Buttons/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./style.css";
+
+const Buttons = ({ tasks, hideDoneTasks }) => (
+    <span className="buttons">
+        {tasks.length > 0 && (
+            <React.Fragment>
+                <button className="buttons__button">
+                    {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
+                </button>
+                <button
+                    className="buttons__button"
+                    disabled={tasks.every(({ done }) => done)}
+                >
+                    Ukończ wszystkie
+                </button>
+            </React.Fragment>
+        )}
+    </span>
+);
+
+export default Buttons;
